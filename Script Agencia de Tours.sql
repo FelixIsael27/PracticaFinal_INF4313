d41e307a6f1cfb5c2aeb68d5b85481cd7b4669f1
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-12-02 20:24:18 -0400

    Preparando los valores de las tablas
    Preparando los valores para las tablas Paises, Destinos y Tours, para cuando realice las migraciones ahora

--- Script Agencia de Tours.sql
+++ Script Agencia de Tours.sql
@@ -38,3 +38,12 @@ CREATE INDEX IX_Destinos_Paises ON Destinos(PaisID);
 CREATE INDEX IX_Tours_Paises ON Tours(PaisID);
 CREATE INDEX IX_Tours_Destinos ON Tours(DestinoID);
 
+INSERT INTO Paises (Nombre)
+VALUES ('República Dominicana'), ('México'), ('España')
+
+INSERT INTO Destinos (Nombre, PaisID, Dias_Duracion, Horas_Duracion)
+VALUES ('Punta Cana', 1, 5, 6), ('Cancún Hotel Zone', 2, 4, 8), ('Madrid Centro Histórico', 3, 3, 4);
+
+INSERT INTO Tours (Nombre, PaisID, DestinoID, Fecha, Hora, Precio)
+VALUES ('Tour Playa Bávaro Premium', 1, 1, '2025-07-15', '09:00', 150.00), ('Tour Isla Mujeres Deluxe', 2, 2, '2025-08-10', '08:30', 220.00),
+('Tour Museo del Prado & Caminata Histórica', 3, 3, '2025-09-05', '10:00', 180.00);

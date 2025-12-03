0136d72d30344eb5ffb6b0c21af7e44beaf9b784
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-12-02 20:45:15 -0400

    Migraciones realizadas

---
+++ AgenciadeTours/AgenciadeTours/Models/AgenciadeTours/Destino.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgenciadeTours.Models.AgenciadeTours;
+
+public partial class Destino
+{
+    public int DestinoId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int PaisId { get; set; }
+
+    public int DiasDuracion { get; set; }
+
+    public int HorasDuracion { get; set; }
+
+    public virtual Paise Pais { get; set; } = null!;
+
+    public virtual ICollection<Tour> Tours { get; set; } = new List<Tour>();
+}
---
+++ AgenciadeTours/AgenciadeTours/Models/AgenciadeTours/Paise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgenciadeTours.Models.AgenciadeTours;
+
+public partial class Paise
+{
+    public int PaisId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Destino> Destinos { get; set; } = new List<Destino>();
+
+    public virtual ICollection<Tour> Tours { get; set; } = new List<Tour>();
+}

6440fc2b4fd99e18796814dd5bc7f3c9ae641e88
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-12-02 21:28:02 -0400

    Eliminando la migración por algunos errores

--- AgenciadeTours/AgenciadeTours/Program.cs
+++ AgenciadeTours/AgenciadeTours/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ADT")));
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
